06d93d55f8963131c490cbfa0ac7df43b03278f8
Baspa <hello@baspa.dev>
2025-08-08 13:29:47 +0200

    fix: names

--- src/UploadcareFieldServiceProvider.php
+++ src/UploadcareFieldServiceProvider.php
@@ -10,7 +10,7 @@ class UploadcareFieldServiceProvider extends PackageServiceProvider
     public function configurePackage(Package $package): void
     {
         $package
-            ->name('backstage-uploadcare-field')
+            ->name('backstage/uploadcare-field')
             ->hasMigrations([
                 '2025_08_08_000000_fix_uploadcare_double_encoded_json',
             ]);
