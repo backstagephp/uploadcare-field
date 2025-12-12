b792a21a0a29cb0ea8598d895c115564a8253cc4
Baspa <10845460+Baspa@users.noreply.github.com>
2025-12-12 13:09:29 +0000

    fix: styling

--- src/Uploadcare.php
+++ src/Uploadcare.php
@@ -19,8 +19,8 @@ use Filament\Schemas\Components\Tabs;
 use Filament\Schemas\Components\Tabs\Tab;
 use Filament\Support\Icons\Heroicon;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Auth;
 
 class Uploadcare extends Base implements FieldContract, HydratesValues
 {
