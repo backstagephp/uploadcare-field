7b6086e6ba83f5e3fd3b89bc7bd7f2d7ba2846b8
Backstage <hello@backstagephp.com>
2025-12-29 12:22:28 +0000

    feat: make user email unique

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -13,7 +13,7 @@ class TestCase extends Orchestra
         parent::setUp();
 
         Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'Backstage\\UploadcareField\\Database\\Factories\\'.class_basename($modelName).'Factory'
+            fn (string $modelName) => 'Backstage\\UploadcareField\\Database\\Factories\\' . class_basename($modelName) . 'Factory'
         );
     }
 
